1997f97f7f70ee6fd5411e45f24fe66bfb7a9c09
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2025-02-08 00:19:23 +0600

    made admin side navbar and contents responsive

--- src/app/(dashboardLayout)/layout.tsx
+++ src/app/(dashboardLayout)/layout.tsx
@@ -4,9 +4,9 @@ import { ReactNode } from "react";
 const layout = ({ children }: { children: ReactNode }) => {
     return (
         <>
-            <div className="flex">
+            <div className="flex flex-col md:flex-row">
                 <AdminSidebarComponent />
-                <div className="w-4/5 mx-auto mt-28">{children}</div>
+                <div className="w-full md:w-4/5 mx-auto mt-28">{children}</div>
             </div>
         </>
     );
--- src/components/admin/adminSidebar.tsx
+++ src/components/admin/adminSidebar.tsx
@@ -6,7 +6,7 @@ const AdminSidebarComponent = () => {
         <div>
             <div className="flex">
                 {/* dashboard sidebar */}
-                <div className="w-24 md:w-64 min-h-screen bg-[#6AAF07] shadow-xl">
+                <div className="w-full md:w-64 min-h-screen bg-[#6AAF07] shadow-xl">
                     <ul className="menu mt-20">
                         <li>
                             <Link className="text-base font-bold text-white border md:border-none border-white mb-4 md:mb-0" href={'/adminDashboard'}>
