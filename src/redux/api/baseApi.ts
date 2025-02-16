c30d312b7306d5133b3261b1ad8d50a52c6fe9aa
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2025-02-17 01:44:52 +0600

    added wrong server live link before, now added right link in base api

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
 
 const baseApiQuery = fetchBaseQuery({
-    baseUrl: "https://apollo-level2-web-dev-batch-3-assignment-6-github.vercel.app/",
+    baseUrl: "https://apollo-level2-web-dev-batch-3-assignment-6-server-github.vercel.app/",
 
     //credentials: "include",
     prepareHeaders: (headers, { getState }) => {
